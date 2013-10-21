d64bc6ca18d21a59d5d13beb8b334922045ae985
Bert Frees <bertfrees@gmail.com>
2013-10-21 19:52:03 +0200

    Register schema and snippets after loading xproc-mode

--- xproc-mode.el
+++ xproc-mode.el
@@ -5,4 +5,12 @@
 (define-derived-mode xproc-mode nxml-mode "XProc"
   "Major mode for editing XProc")
 
+(push (expand-file-name "schema/schemas.xml" (file-name-directory load-file-name))
+      rng-schema-locating-files)
+
+(push (expand-file-name "snippets" (file-name-directory load-file-name))
+      yas/snippet-dirs)
+
+(yas-reload-all)
+
 (provide 'xproc-mode)
